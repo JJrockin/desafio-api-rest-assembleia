7a12548aa9ed0f3467eb7f06aac9d2785d466185
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-06-21 15:48:29 -0300

    First commit, adding project

---
+++ api-assembleia/src/main/java/com/jjrockin/api/assembleia/ApiAssembleiaApplication.java
@@ -0,0 +1,13 @@
+package com.jjrockin.api.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAssembleiaApplication.class, args);
+	}
+
+}
---
+++ api-assembleia/src/test/java/com/jjrockin/api/assembleia/ApiAssembleiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjrockin.api.assembleia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAssembleiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
